af04128e9c9ee62e2828d3960ccfe9ebe6dee4cc
Prabuselvan2022 <prabuelamuruguselvan@gmail.com>
2023-05-14 12:43:52 +0530

    extent reports

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,10 +8,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -21,9 +31,16 @@ public class BaseTest {
 	public static FileReader filereader;
 	public static FileReader filereader1;
 	public static Properties props = new Properties();
+	public static ExtentHtmlReporter HtmlReporter;
+	public static ExtentReports Report;
+	public static ExtentTest Test;
+
+			
+	
 	
 	@BeforeMethod
 	public void setup() throws IOException {
+
 		
 		if(driver==null) { //checking the driver is null 
 			System.out.println("directory is "+ System.getProperty("user.dir"));
@@ -31,6 +48,18 @@ public class BaseTest {
 			filereader1 = new FileReader(System.getProperty("user.dir")+"/src/test/resources/Configfiles/location.properties");
 			props.load(filereader);
 			props.load(filereader1);
+			HtmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/Reports/testReport.html");
+			Report = new ExtentReports();
+			Report.attachReporter(HtmlReporter);
+			Report.setSystemInfo("OS", "MacOSX");
+			Report.setSystemInfo("Browser", props.getProperty("browser"));
+			HtmlReporter.config().setChartVisibilityOnOpen(true);
+			HtmlReporter.config().setDocumentTitle("Extent Report Demo");
+			HtmlReporter.config().setReportName("Test Report");
+			HtmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+			HtmlReporter.config().setTheme(Theme.STANDARD);
+			HtmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+
 		}
 		
 		if(props.getProperty("browser").equalsIgnoreCase("chrome")) {
@@ -53,8 +82,24 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void teardown() {
+	public void teardown(ITestResult result) {
+		
+		System.out.println("result.getStatus() "+ result.getStatus());
+		 if(result.getStatus() == ITestResult.FAILURE) {
+			 Test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
+			 Test.fail(result.getThrowable());
+	        }
+	        else if(result.getStatus() == ITestResult.SUCCESS) {
+	        	Test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" PASSED ", ExtentColor.GREEN));
+	        }
+	        else {
+	        	Test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" SKIPPED ", ExtentColor.ORANGE));
+	        	Test.skip(result.getThrowable());
+	        }
+		 Report.flush();
 		driver.close();
 		System.out.println("closed the browser ");
 	}
+	
+
 }
--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -3,6 +3,8 @@ package testcases;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 import base.BaseTest;
 import pageObjects.BSSignUpPage;
 
@@ -13,16 +15,27 @@ public class TestCase2 extends BaseTest {
 
 	@Test
 	public void verifySignInPage() {
+		Test = Report.createTest("verifySignInPage ", "PASSED test case");
+
 		bs = new BSSignUpPage(driver);
 		bs.clickSignIn();
+		Test.log(Status.INFO, "Clicked on SignIn Page");
 		bs.verifySignInPage();
+		Test.log(Status.INFO, "Verified the SignIn page is loaded");
+		Report.flush();
+		
 	}
 
 	@Test
 	public void pricingPage() {
+		Test = Report.createTest("pricingPage ", "PASSED test case");
 		bs = new BSSignUpPage(driver);
 		bs.clickPricingPage();
+		Test.log(Status.INFO, "Clicked on Pricing Page");
 		bs.verifyPricingPage();
+		Test.log(Status.INFO, "Verified the pricing page is loaded");
+		Report.flush();
+		
 	}
 
 }
